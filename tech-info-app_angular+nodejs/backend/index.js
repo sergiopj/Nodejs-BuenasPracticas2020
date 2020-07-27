58e05a3a6343c35a452c36f8f987f2d4101c5efe
sergiopj <sergiopjaranda@gmail.com>
2020-07-27 15:17:49 +0200

    routes temporales definidas

--- tech-info-app_angular+nodejs/backend/index.js
+++ tech-info-app_angular+nodejs/backend/index.js
@@ -1,7 +1,15 @@
-const server = require('./server');
-const { PORT } = require('./config');
+const server = require("./server");
+const { PORT, MONGO_URI } = require("./config");
+const mongoose = require("mongoose");
 
-
-server.listen(PORT, () => {
-    console.log(`TechApp backend running on port ${PORT}`);
-});
+mongoose
+  .connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log("Database connection establised");
+    server.listen(PORT, () => {
+      console.log(`TechApp backend running on port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("Error when try connect to bd -- error: ", err);
+  });
--- tech-info-app_angular+nodejs/backend/server/index.js
+++ tech-info-app_angular+nodejs/backend/server/index.js
@@ -1,4 +1,60 @@
 const express = require('express');
 const server = express();
+const cors = require('cors');
+const { Technology } = require('../models'); 
+
+
+// config para tratar con json en las peticiones
+server.use(express.json());
+
+// hacemos publico el dir public
+server.use(express.static(__dirname + '/../public'));
+
+// cors config
+server.use(cors());
+
+
+// end points TODO migrarlo a otro fichero
+server.get('/api/technologies', async (req, res) => {
+
+    let technologies = await Technology.find();
+
+    // se recorre con map para modificar el resultado
+    technologies = technologies.map( (technology) => {
+        // ruta correcta de cada logo en nuestra maquina
+        technology.logo = `${req.protocol}://${req.headers.host}/img/${technology.logo}`;
+        return technology;
+    })
+
+    // recorremos con map y filtramos lo que queremos devolver
+    // por defecto el status code es 200
+    res.send({
+        ok: true,
+        technologies
+    });
+
+});
+
+server.get('/api/technology/:id', async (req, res) => {
+
+    const id = req.params.id;
+
+    let technology = await Technology.findById(id);
+
+    console.log(id, technology)
+
+    // se recorre con map para modificar el resultado
+    technology.logo = `${req.protocol}://${req.headers.host}/img/${technology.logo}`;
+    
+
+    // recorremos con map y filtramos lo que queremos devolver
+    // por defecto el status code es 200
+    res.send({
+        ok: true,
+        technology
+    });
+
+});
+
 
 module.exports = server;
